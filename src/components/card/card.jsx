7a80b651bdab9cf7a00eea5eb05d379c115efae0
Edward Didyk <edwarddidyk@MacBook-Air-Edward.local>
2023-03-04 15:23:13 +0200

    puffy commit fix pics public url

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -2,15 +2,15 @@
 import './card.css';
 
 function Card(props) {
-    const { id, name, website } = props.monster;
-    return (
-      <div className='card-container' key={id}>
-        <img src={'imges/puf' + id + '.png'} alt={`monster ${name}`} className='puffy-img' />
-        <h2>{name}</h2>
-        <p>{website}</p>
-      </div>
-    )
-  }
+  const { id, name, website } = props.monster;
+  return (
+    <div className='card-container' key={id}>
+      <img src={process.env.PUBLIC_URL + '/imges/puf' + id + '.png'} alt={`monster ${name}`} className='puffy-img' />
+      <h2>{name}</h2>
+      <p>{website}</p>
+    </div>
+  )
+}
 
 export default Card;
 
@@ -30,4 +30,3 @@ export default Card;
 //     )
 //   }
 // }
-
